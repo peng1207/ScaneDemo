8f1867a4915e3f4f687e274b61e9397465b39647
huangshp@okdeer.com <huangshp@okdeer.com>
2018-05-31 09:35:58 +0800

    Initial Commit

---
+++ ScaneDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScaneDemo
+//
+//  Created by okdeer on 2018/5/31.
+//  Copyright © 2018年 okdeer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
